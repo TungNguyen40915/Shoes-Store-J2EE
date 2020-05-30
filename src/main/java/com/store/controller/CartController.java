cb1abc31712d3345d548b6beba69e17d9f1d6f35
Thu Nguyen <hoaithu7604@gmail.com>
2020-05-30 09:41:21 +0900

    add sync cart api

--- src/main/java/com/store/controller/CartController.java
+++ src/main/java/com/store/controller/CartController.java
@@ -126,4 +126,47 @@ public class CartController {
         }
         return res;
     }
+
+    @POST
+    @LoginRequired
+    @Path("/sync")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response syncCart(
+            CartUpdateModel[] cartItems,
+            @Context ContainerRequestContext requestContext) {
+        String token = requestContext.getHeaderString(Constant.AUTH_HEADER).substring(Constant.AUTH_BEARER.length());
+        String username = JWTProvider.getUserNameFromJwtToken(token);
+        //List<UpdateCartItem> updateCartItems = Util.handleConvertLisUpdateItem(list);
+        Response res = new Response();
+        try {
+            List<CartItem> oldList = CartDAO.getAllCartItemsByCartId(CartDAO.getCartIdByUserName(username));
+
+            //empty cart lazy way
+            //TODO: implement empty cart method for DAO
+            for (CartItem i: oldList){
+                CartDAO.updateCart(username,i.getStockID(),0);
+            }
+
+            for (CartUpdateModel u: cartItems) {
+                CartDAO.addItemToCart(username,u.getStockId(),u.getQuantity());
+            }
+            List<CartItem> items = CartDAO.getAllCartItemsByCartId(CartDAO.getCartIdByUserName(username));
+            List<CartItemDTO> itemDTOS = new ArrayList<CartItemDTO>();
+            res.setCode("OK");
+            res.setMsg("Updated Successfully");
+            for (CartItem s: items) {
+                itemDTOS.add(CartConverter.ConvertCartItemEntityToCartItemDTO(s));
+            }
+            res.setData(mapper.writeValueAsString(itemDTOS));
+            res.setTotalRecords(items.size());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.setCode("FAIL");
+            res.setMsg("Something Went Wrong");
+            res.setData("[]");
+            res.setTotalRecords(0);
+        }
+        return res;
+    }
 }
